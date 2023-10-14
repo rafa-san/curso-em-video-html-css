5e5852eddc6543cc63b7cf542b923ce2f9fd3dda
Rafael <88090582+rafa-san@users.noreply.github.com>
2023-10-14 12:05:14 -0300

    Atualizações:
    Atualização de links e melhorias nos arquivos .css

--- scripts/menu-responsivo.js
+++ scripts/menu-responsivo.js
@@ -1,8 +1,14 @@
 function menu() {
-  let x = document.getElementById("topnavResponsivo");
-  if (x.className === "topnav") {
-    x.className += " responsivo";
+  let menu = document.getElementById("topnavResponsivo");
+  if (menu.className === "topnav") {
+    menu.className += " responsivo";
   } else {
-    x.className = "topnav";
+    menu.className = "topnav";
   }
 }
+
+
+
+
+
+
