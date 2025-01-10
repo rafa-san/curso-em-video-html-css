086c5ee8b3090328fe796360b15330cd12052834
Rafael Santana <88090582+rafa-san@users.noreply.github.com>
2025-01-10 09:56:25 -0300

    chore: removi um documento não mais utilizado

--- scripts/modulo-04-exe-02.js
+++
@@ -1,7 +0,0 @@
-const botao = document.getElementById('botao');
-const iFrame = document.getElementById('iFrame');
-
-botao.addEventListener('click', exibirFrame);
-function exibirFrame() {
-  iFrame.style.display = 'block';
-}
